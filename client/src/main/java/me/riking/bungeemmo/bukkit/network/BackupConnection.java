568516d233049ee72a8267facc1d4475042b659f
riking <rikingcoding@gmail.com>
2013-07-31 12:57:46 -0700

    Comment out code in BackupConnection

--- client/src/main/java/me/riking/bungeemmo/bukkit/network/BackupConnection.java
+++ client/src/main/java/me/riking/bungeemmo/bukkit/network/BackupConnection.java
@@ -4,7 +4,10 @@ import java.net.InetAddress;
 import java.net.Socket;
 
 public class BackupConnection {
+    /*
+     * Commented out to remove compile errors.
     public BackupConnection(InetAddress proxy, int port) {
         new Socket(proxy, 33333);
     }
+    */
 }
